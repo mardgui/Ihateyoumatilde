a6647c84c64c3acbb601e4e93b1b9648a3da740a
Quentin Guimard <quentin.guimard@etu.unice.fr>
2018-12-18 09:46:14 +0100

    there was an attempt

--- src/main/java/be/uclouvain/lingi2252/smarthome/Main.java
+++ src/main/java/be/uclouvain/lingi2252/smarthome/Main.java
@@ -1,32 +1,15 @@
 package be.uclouvain.lingi2252.smarthome;
 
-import be.uclouvain.lingi2252.groupN.House;
-import be.uclouvain.lingi2252.groupN.Room;
 import be.uclouvain.lingi2252.groupN.parameterization.Parameterization;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
-import javafx.stage.Stage;
-import javafx.scene.Group;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.TilePane;
 import javafx.stage.Stage;
 
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class Main extends Application {
-
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -36,30 +19,9 @@ public class Main extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader();
         Parent rootNode = (Parent) fxmlLoader.load(getClass().getResourceAsStream(fxmlFile));
 
-        House house = House.getInstance();
-        /*GridPane grid = new GridPane();
-
-        List roomList = house.getRooms();
-
-        System.out.println(roomList.size());
-
-        for (int i = 0; i < roomList.size(); i++){
-            Rectangle rect = new Rectangle(i+1, i+2, 50, 50 );
-            rect.setFill(Color.WHITE);
-            grid.add(rect, 1, i, 1, 1);
-            System.out.println("Print one rect");
-        }
-
-        grid.setAlignment(Pos.CENTER);
-        */
-
-        primaryStage.setTitle("GUI");
-        primaryStage.setScene(new Scene(rootNode, 400, 400));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(rootNode, 600, 400));
         primaryStage.show();
-
-
-
-
     }
 
 
